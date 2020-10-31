3f83b5b4dbbb3010c413dcd9260e19fb59088b2f
Viviane <ramosluzviviane@gmail.com>
2020-10-31 11:55:25 -0300

    end fecth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Landing from './pages/landing'
 
-
 function App() {
   return (
     <Landing/>
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,10 +1,9 @@
-import React , { FormEvent, FormEventHandler, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import axios from 'axios'
 import '../styles/landing.css'
 import lupa from '../assets/lupa.svg'
-import { cleanup } from '@testing-library/react'
-
+import close from '../assets/close.svg'
 
 interface User {
     avatar_url: string;
@@ -12,25 +11,35 @@ interface User {
     bio: string;
     location: string;
     html_url:string;
+    blog: string;
+    twitter_username: string;
+    company: string;
+    hireable: boolean;
 }
+
 
 export default function Landing(){
     const [ user, setUser ] = useState('')
+    const [ url, setUrl ] = useState('https://api.github.com/users/octocat')
     const [ dataUser, setDataUser ] = useState<User>()
-
-   
-       async function fecthApiGitHub(){
-            await axios.get(`https://api.github.com/users/${user}`)
-             .then( res => {
-                 setDataUser(res.data)
-             })
-             .catch(error => console.error("Erro na requisição", error))
-        }
-
-       function clean(){
-           setUser("")
+    const [ isLoading, setIsLoading ] = useState(false)
+  
+    useEffect(()=>{
+      const fecthApiGitHub = async () =>{
+            setIsLoading(true)
+            await axios.get(url)
+            .then( ({data}) => {
+                setDataUser(data)  
+                setIsLoading(false) 
+            })
+            .catch(error => {
+               alert('Usuário não encontrado!')
+                console.error("Erro na requisição", error)
+            }) 
        }
-
+       fecthApiGitHub()
+    }, [url])
+       
 
     return (
         <div className="main">
@@ -39,23 +48,38 @@ export default function Landing(){
             <main >
                 <span>Usuário:</span>
                 <div className="contInput">
-                    <input type="text" name={user} onChange={ e => setUser(e.target.value)} 
-                    onClick={clean}/>
-                    <button onClick={fecthApiGitHub}><img src={lupa} alt="Pesquisa"/></button>
+                    <input type="text"
+                    value={user} 
+                    onChange={ e => {
+                        setUser(e.target.value)
+                    }} 
+                    />
+
+                    <button 
+                        onClick={()=> setUrl(`https://api.github.com/users/${user}`)}>
+                        <img src={lupa} alt="Pesquisa"/>
+                    </button>
                 </div>
             </main>
 
-        { !dataUser ? null : (
-             <div className="contentUser">
-             <img className="imgBio" src={dataUser!.avatar_url} alt={`Imagem de ${dataUser!.name}`}/>
-             <h2>{dataUser!.name}</h2>
-             <p>{dataUser!.bio}</p>
-             <p>{dataUser!.location}</p>
-             <a href={dataUser!.html_url} target="_blank" rel="noreferrer noopener">Visitar </a>
-         </div>
-         ) }
-           
-        </div>
-        
+            
+
+            { isLoading ? (
+                <div className="spinner"></div>
+            ) : (
+            <div className="contentUser">
+            <img className="imgBio" src={dataUser?.avatar_url} alt={`Imagem de ${dataUser?.name}`}/>
+            <h2>{dataUser?.name}</h2>
+            <p>{dataUser?.bio}</p>
+            <p>{dataUser?.location}</p>
+            <a href={dataUser?.html_url} target="_blank" rel="noreferrer noopener">Visitar </a>
+            <a href={dataUser?.blog} target="_blank" rel="noreferrer noopener">Blog</a>
+            <a href={dataUser?.twitter_username} target="_blank" rel="noreferrer noopener">Twitter</a>
+            <p>{dataUser?.company}</p>
+            
+            </div>    
+            ) }
+            
+    </div>
     )
 }
